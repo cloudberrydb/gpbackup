cloudberrydb/gpbackup

commit 43ca011c507800ad3b816f8d3d37a02ba8591180

Use directory name from pg_filespace_entry with --backupdir
Previously, gpbackup hardcoded "gpseg" as the segment prefix for segment directories
created when using the --backupdir flag.  This commit modifies gpbackup to use the
names of the cluster's segment directories for consistency.

Signed-off-by: Chris Hajas <chajas@pivotal.io>

utils/cluster.go
@@ -54,6 +54,7 @@ type Cluster struct {
 	SegDirMap              map[int]string
 	SegHostMap             map[int]string
 	UserSpecifiedBackupDir string
+	UserSpecifiedSegPrefix string
 	Timestamp              string
 	Executor
 }
@@ -72,11 +73,12 @@ func (cluster *Cluster) IsUserSpecifiedBackupDir() bool {
 	return cluster.UserSpecifiedBackupDir != ""
 }
 
-func NewCluster(segConfigs []SegConfig, userSpecifiedBackupDir string, timestamp string) Cluster {
+func NewCluster(segConfigs []SegConfig, userSpecifiedBackupDir string, timestamp string, userSegPrefix string) Cluster {
 	cluster := Cluster{}
 	cluster.SegHostMap = make(map[int]string, 0)
 	cluster.SegDirMap = make(map[int]string, 0)
 	cluster.UserSpecifiedBackupDir = userSpecifiedBackupDir
+	cluster.UserSpecifiedSegPrefix = userSegPrefix
 	cluster.Timestamp = timestamp
 	for _, seg := range segConfigs {
 		cluster.ContentIDs = append(cluster.ContentIDs, seg.ContentID)
@@ -214,7 +216,7 @@ func (cluster *Cluster) GetHostForContent(contentID int) string {
 
 func (cluster *Cluster) GetDirForContent(contentID int) string {
 	if cluster.IsUserSpecifiedBackupDir() {
-		segDir := fmt.Sprintf("gpseg%d", contentID)
+		segDir := fmt.Sprintf("%s%d", cluster.UserSpecifiedSegPrefix, contentID)
 		return path.Join(cluster.UserSpecifiedBackupDir, segDir, "backups", cluster.Timestamp[0:8], cluster.Timestamp)
 	}
 	return path.Join(cluster.SegDirMap[contentID], "backups", cluster.Timestamp[0:8], cluster.Timestamp)
@@ -233,7 +235,7 @@ func (cluster *Cluster) GetTableBackupFilePathForCopyCommand(tableOid uint32) st
 	}
 	baseDir := "<SEG_DATA_DIR>"
 	if cluster.IsUserSpecifiedBackupDir() {
-		baseDir = path.Join(cluster.UserSpecifiedBackupDir, "gpseg<SEGID>")
+		baseDir = path.Join(cluster.UserSpecifiedBackupDir, fmt.Sprintf("%s<SEGID>", cluster.UserSpecifiedSegPrefix))
 	}
 	return path.Join(baseDir, "backups", cluster.Timestamp[0:8], cluster.Timestamp, backupFilePath)
 }
@@ -334,6 +336,26 @@ ORDER BY s.content;`
 	err := connection.Select(&results, query)
 	CheckError(err)
 	return results
+}
+
+func GetSegPrefix(connection *DBConn) string {
+	query := "SELECT fselocation FROM pg_filespace_entry WHERE fsedbid = 1;"
+	result := ""
+	err := connection.Get(&result, query)
+	CheckError(err)
+	_, segPrefix := path.Split(result)
+	segPrefix = segPrefix[:len(segPrefix)-2] // Remove "-1" segment ID from string
+	return segPrefix
+}
+
+func ParseSegPrefix(backupDir string) string {
+	masterDir, err := System.Glob(fmt.Sprintf("%s/*-1", backupDir))
+	if err != nil || len(masterDir) == 0 {
+		logger.Fatal(err, "Master backup directory in %s missing or inaccessible", backupDir)
+	}
+	_, segPrefix := path.Split(masterDir[0])
+	segPrefix = segPrefix[:len(segPrefix)-2] // Remove "-1" segment ID from string
+	return segPrefix
 }
 
 func ConstructSSHCommand(host string, cmd string) []string {

utils/cluster_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/user"
+	"path/filepath"
 
 	sqlmock "gopkg.in/DATA-DOG/go-sqlmock.v1"
 
@@ -30,7 +31,7 @@ var _ = Describe("utils/cluster tests", func() {
 		utils.System.CurrentUser = func() (*user.User, error) { return &user.User{Username: "testUser", HomeDir: "testDir"}, nil }
 		utils.System.Hostname = func() (string, error) { return "testHost", nil }
 		testExecutor = &testutils.TestExecutor{}
-		testCluster = utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne, remoteSegOne}, "", "20170101010101")
+		testCluster = utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne, remoteSegOne}, "", "20170101010101", "gpseg")
 		testCluster.Executor = testExecutor
 	})
 	Describe("ConstructSSHCommand", func() {
@@ -78,7 +79,7 @@ var _ = Describe("utils/cluster tests", func() {
 	})
 	Describe("GenerateSSHCommandMap", func() {
 		It("Returns a map of ssh commands for the master, including master", func() {
-			cluster := utils.NewCluster([]utils.SegConfig{masterSeg}, "", "20170101010101")
+			cluster := utils.NewCluster([]utils.SegConfig{masterSeg}, "", "20170101010101", "gpseg")
 			commandMap := cluster.GenerateSSHCommandMap(true, func(_ int) string {
 				return "ls"
 			})
@@ -86,14 +87,14 @@ var _ = Describe("utils/cluster tests", func() {
 			Expect(commandMap[-1]).To(Equal([]string{"bash", "-c", "ls"}))
 		})
 		It("Returns a map of ssh commands for the master, excluding master", func() {
-			cluster := utils.NewCluster([]utils.SegConfig{masterSeg}, "", "20170101010101")
+			cluster := utils.NewCluster([]utils.SegConfig{masterSeg}, "", "20170101010101", "gpseg")
 			commandMap := cluster.GenerateSSHCommandMap(false, func(_ int) string {
 				return "ls"
 			})
 			Expect(len(commandMap)).To(Equal(0))
 		})
 		It("Returns a map of ssh commands for one segment, including master", func() {
-			cluster := utils.NewCluster([]utils.SegConfig{remoteSegOne}, "", "20170101010101")
+			cluster := utils.NewCluster([]utils.SegConfig{remoteSegOne}, "", "20170101010101", "gpseg")
 			commandMap := cluster.GenerateSSHCommandMap(true, func(_ int) string {
 				return "ls"
 			})
@@ -101,7 +102,7 @@ var _ = Describe("utils/cluster tests", func() {
 			Expect(commandMap[1]).To(Equal([]string{"ssh", "-o", "StrictHostKeyChecking=no", "testUser@remotehost1", "ls"}))
 		})
 		It("Returns a map of ssh commands for one segment, excluding master", func() {
-			cluster := utils.NewCluster([]utils.SegConfig{remoteSegOne}, "", "20170101010101")
+			cluster := utils.NewCluster([]utils.SegConfig{remoteSegOne}, "", "20170101010101", "gpseg")
 			commandMap := cluster.GenerateSSHCommandMap(false, func(_ int) string {
 				return "ls"
 			})
@@ -109,7 +110,7 @@ var _ = Describe("utils/cluster tests", func() {
 			Expect(commandMap[1]).To(Equal([]string{"ssh", "-o", "StrictHostKeyChecking=no", "testUser@remotehost1", "ls"}))
 		})
 		It("Returns a map of ssh commands for two segments on the same host, including master", func() {
-			cluster := utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne}, "", "20170101010101")
+			cluster := utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne}, "", "20170101010101", "gpseg")
 			commandMap := cluster.GenerateSSHCommandMap(true, func(_ int) string {
 				return "ls"
 			})
@@ -118,7 +119,7 @@ var _ = Describe("utils/cluster tests", func() {
 			Expect(commandMap[0]).To(Equal([]string{"ssh", "-o", "StrictHostKeyChecking=no", "testUser@localhost", "ls"}))
 		})
 		It("Returns a map of ssh commands for two segments on the same host, excluding master", func() {
-			cluster := utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne}, "", "20170101010101")
+			cluster := utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne}, "", "20170101010101", "gpseg")
 			commandMap := cluster.GenerateSSHCommandMap(false, func(_ int) string {
 				return "ls"
 			})
@@ -126,7 +127,7 @@ var _ = Describe("utils/cluster tests", func() {
 			Expect(commandMap[0]).To(Equal([]string{"ssh", "-o", "StrictHostKeyChecking=no", "testUser@localhost", "ls"}))
 		})
 		It("Returns a map of ssh commands for three segments on different hosts", func() {
-			cluster := utils.NewCluster([]utils.SegConfig{localSegOne, remoteSegOne, remoteSegTwo}, "", "20170101010101")
+			cluster := utils.NewCluster([]utils.SegConfig{localSegOne, remoteSegOne, remoteSegTwo}, "", "20170101010101", "gpseg")
 			commandMap := cluster.GenerateSSHCommandMap(false, func(contentID int) string {
 				return fmt.Sprintf("mkdir -p %s", cluster.GetDirForContent(contentID))
 			})
@@ -138,7 +139,7 @@ var _ = Describe("utils/cluster tests", func() {
 	})
 	Describe("GenerateSSHCommandMapForCluster", func() {
 		It("includes master in the command map", func() {
-			cluster := utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne}, "", "20170101010101")
+			cluster := utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne}, "", "20170101010101", "gpseg")
 			commandMap := cluster.GenerateSSHCommandMapForCluster(func(_ int) string {
 				return "ls"
 			})
@@ -149,7 +150,7 @@ var _ = Describe("utils/cluster tests", func() {
 	})
 	Describe("GenerateSSHCommandMapForSegments", func() {
 		It("excludes master from the command map", func() {
-			cluster := utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne}, "", "20170101010101")
+			cluster := utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne}, "", "20170101010101", "gpseg")
 			commandMap := cluster.GenerateSSHCommandMapForSegments(func(_ int) string {
 				return "ls"
 			})
@@ -159,7 +160,7 @@ var _ = Describe("utils/cluster tests", func() {
 	})
 	Describe("GenerateFileVerificationCommandMap", func() {
 		It("creates a command map for segments only", func() {
-			cluster := utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne, remoteSegOne}, "", "20170101010101")
+			cluster := utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne, remoteSegOne}, "", "20170101010101", "gpseg")
 			commandMap := cluster.GenerateFileVerificationCommandMap(13)
 
 			Expect(len(commandMap)).To(Equal(2))
@@ -226,12 +227,12 @@ var _ = Describe("utils/cluster tests", func() {
 	})
 	Describe("LogFatalError", func() {
 		It("logs an error for 1 segment", func() {
-			cluster := utils.NewCluster(nil, "", "20170101010101")
+			cluster := utils.NewCluster(nil, "", "20170101010101", "gpseg")
 			defer testutils.ShouldPanicWithMessage("Error occurred on 1 segment. See gbytes.Buffer for a complete list of segments with errors.")
 			cluster.LogFatalError("Error occurred", 1)
 		})
 		It("logs an error for more than 1 segment", func() {
-			cluster := utils.NewCluster(nil, "", "20170101010101")
+			cluster := utils.NewCluster(nil, "", "20170101010101", "gpseg")
 			defer testutils.ShouldPanicWithMessage("Error occurred on 2 segments. See gbytes.Buffer for a complete list of segments with errors.")
 			cluster.LogFatalError("Error occurred", 2)
 		})
@@ -242,7 +243,7 @@ var _ = Describe("utils/cluster tests", func() {
 		localSegTwo := utils.SegConfig{ContentID: 1, Hostname: "localhost", DataDir: "/data/gpseg1"}
 		remoteSegTwo := utils.SegConfig{ContentID: 1, Hostname: "remotehost", DataDir: "/data/gpseg1"}
 		It("returns content dir for a single-host, single-segment nodes", func() {
-			cluster := utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne}, "", "20170101010101")
+			cluster := utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne}, "", "20170101010101", "gpseg")
 			Expect(len(cluster.GetContentList())).To(Equal(2))
 			Expect(cluster.GetDirForContent(-1)).To(Equal("/data/gpseg-1/backups/20170101/20170101010101"))
 			Expect(cluster.GetHostForContent(-1)).To(Equal("localhost"))
@@ -250,7 +251,7 @@ var _ = Describe("utils/cluster tests", func() {
 			Expect(cluster.GetHostForContent(0)).To(Equal("localhost"))
 		})
 		It("sets up the configuration for a single-host, multi-segment cluster", func() {
-			cluster := utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne, localSegTwo}, "", "20170101010101")
+			cluster := utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne, localSegTwo}, "", "20170101010101", "gpseg")
 			Expect(len(cluster.GetContentList())).To(Equal(3))
 			Expect(cluster.GetDirForContent(-1)).To(Equal("/data/gpseg-1/backups/20170101/20170101010101"))
 			Expect(cluster.GetHostForContent(-1)).To(Equal("localhost"))
@@ -260,7 +261,7 @@ var _ = Describe("utils/cluster tests", func() {
 			Expect(cluster.GetHostForContent(1)).To(Equal("localhost"))
 		})
 		It("sets up the configuration for a multi-host, multi-segment cluster", func() {
-			cluster := utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne, remoteSegTwo}, "", "20170101010101")
+			cluster := utils.NewCluster([]utils.SegConfig{masterSeg, localSegOne, remoteSegTwo}, "", "20170101010101", "gpseg")
 			Expect(len(cluster.GetContentList())).To(Equal(3))
 			Expect(cluster.GetDirForContent(-1)).To(Equal("/data/gpseg-1/backups/20170101/20170101010101"))
 			Expect(cluster.GetHostForContent(-1)).To(Equal("localhost"))
@@ -270,37 +271,37 @@ var _ = Describe("utils/cluster tests", func() {
 			Expect(cluster.GetHostForContent(1)).To(Equal("remotehost"))
 		})
 		It("returns the content directory based on the user specified path", func() {
-			cluster := utils.NewCluster([]utils.SegConfig{masterSeg}, "/foo/bar", "20170101010101")
+			cluster := utils.NewCluster([]utils.SegConfig{masterSeg}, "/foo/bar", "20170101010101", "gpseg")
 			Expect(cluster.GetDirForContent(-1)).To(Equal("/foo/bar/gpseg-1/backups/20170101/20170101010101"))
 		})
 	})
 	Describe("GetTableBackupFilePathForCopyCommand()", func() {
 		It("returns table file path for copy command", func() {
-			cluster := utils.NewCluster(nil, "", "20170101010101")
+			cluster := utils.NewCluster(nil, "", "20170101010101", "gpseg")
 			Expect(cluster.GetTableBackupFilePathForCopyCommand(1234)).To(Equal("<SEG_DATA_DIR>/backups/20170101/20170101010101/gpbackup_<SEGID>_20170101010101_1234"))
 		})
 		It("returns table file path for copy command based on user specified path", func() {
-			cluster := utils.NewCluster(nil, "/foo/bar", "20170101010101")
+			cluster := utils.NewCluster(nil, "/foo/bar", "20170101010101", "gpseg")
 			Expect(cluster.GetTableBackupFilePathForCopyCommand(1234)).To(Equal("/foo/bar/gpseg<SEGID>/backups/20170101/20170101010101/gpbackup_<SEGID>_20170101010101_1234"))
 		})
 	})
 	Describe("GetReportFilePath", func() {
 		It("returns report file path", func() {
-			cluster := utils.NewCluster([]utils.SegConfig{masterSeg}, "", "20170101010101")
+			cluster := utils.NewCluster([]utils.SegConfig{masterSeg}, "", "20170101010101", "gpseg")
 			Expect(cluster.GetReportFilePath()).To(Equal("/data/gpseg-1/backups/20170101/20170101010101/gpbackup_20170101010101_report"))
 		})
 		It("returns report file path based on user specified path", func() {
-			cluster := utils.NewCluster(nil, "/foo/bar", "20170101010101")
+			cluster := utils.NewCluster(nil, "/foo/bar", "20170101010101", "gpseg")
 			Expect(cluster.GetReportFilePath()).To(Equal("/foo/bar/gpseg-1/backups/20170101/20170101010101/gpbackup_20170101010101_report"))
 		})
 	})
 	Describe("GetTableBackupFilePath", func() {
 		It("returns table file path", func() {
-			cluster := utils.NewCluster([]utils.SegConfig{masterSeg}, "", "20170101010101")
+			cluster := utils.NewCluster([]utils.SegConfig{masterSeg}, "", "20170101010101", "gpseg")
 			Expect(cluster.GetTableBackupFilePath(-1, 1234)).To(Equal("/data/gpseg-1/backups/20170101/20170101010101/gpbackup_-1_20170101010101_1234"))
 		})
 		It("returns table file path based on user specified path", func() {
-			cluster := utils.NewCluster([]utils.SegConfig{masterSeg}, "/foo/bar", "20170101010101")
+			cluster := utils.NewCluster([]utils.SegConfig{masterSeg}, "/foo/bar", "20170101010101", "gpseg")
 			Expect(cluster.GetTableBackupFilePath(-1, 1234)).To(Equal("/foo/bar/gpseg-1/backups/20170101/20170101010101/gpbackup_-1_20170101010101_1234"))
 		})
 	})
@@ -371,6 +372,27 @@ var _ = Describe("utils/cluster tests", func() {
 			testCluster.Executor = testExecutor
 			defer testutils.ShouldPanicWithMessage("Unable to create directories on 1 segment")
 			testCluster.CreateBackupDirectoriesOnAllHosts()
+		})
+	})
+	Describe("ParseSegPrefix", func() {
+		AfterEach(func() {
+			utils.System.Glob = filepath.Glob
+		})
+		It("returns segment prefix from directory path if master backup directory exists", func() {
+			utils.System.Glob = func(pattern string) (matches []string, err error) { return []string{"/tmp/foo/gpseg-1"}, nil }
+			Expect(utils.ParseSegPrefix("/tmp/foo")).To(Equal("gpseg"))
+		})
+		It("panics if master backup directory does not exist", func() {
+			utils.System.Glob = func(pattern string) (matches []string, err error) { return []string{}, nil }
+			defer testutils.ShouldPanicWithMessage("Master backup directory in /tmp/foo missing or inaccessible")
+			Expect(utils.ParseSegPrefix("/tmp/foo")).To(Equal("gpseg"))
+		})
+		It("panics if there is an error accessing master backup directory", func() {
+			utils.System.Glob = func(pattern string) (matches []string, err error) {
+				return []string{""}, os.ErrPermission
+			}
+			defer testutils.ShouldPanicWithMessage("Master backup directory in /tmp/foo missing or inaccessible")
+			Expect(utils.ParseSegPrefix("/tmp/foo")).To(Equal("gpseg"))
 		})
 	})
 })

backup/backup.go
@@ -64,8 +64,9 @@ func DoSetup() {
 
 	segConfig := utils.GetSegmentConfiguration(connection)
 	timestamp := utils.CurrentTimestamp()
+	segPrefix := utils.GetSegPrefix(connection)
 	utils.CreateBackupLockFile(timestamp)
-	globalCluster = utils.NewCluster(segConfig, *backupDir, timestamp)
+	globalCluster = utils.NewCluster(segConfig, *backupDir, timestamp, segPrefix)
 	globalCluster.CreateBackupDirectoriesOnAllHosts()
 	globalTOC = &utils.TOC{}
 	globalTOC.InitializeEntryMapFromCluster(globalCluster)

integration/integration_suite_test.go
@@ -50,7 +50,7 @@ var _ = BeforeSuite(func() {
 	testutils.AssertQueryRuns(connection, "DROP PROTOCOL IF EXISTS gphdfs")
 	testutils.AssertQueryRuns(connection, `SET standard_conforming_strings TO "on"`)
 	segConfig := utils.GetSegmentConfiguration(connection)
-	cluster := utils.NewCluster(segConfig, "/tmp/test_filespace", "20170101010101")
+	cluster := utils.NewCluster(segConfig, "/tmp/test_filespace", "20170101010101", "gpseg")
 	setupTestFilespace(cluster)
 })
 

restore/restore.go
@@ -71,7 +71,8 @@ func DoSetup() {
 
 	logger.Verbose("Gathering information on backup directories")
 	segConfig := utils.GetSegmentConfiguration(connection)
-	globalCluster = utils.NewCluster(segConfig, *backupDir, *timestamp)
+	globalCluster = utils.NewCluster(segConfig, *backupDir, *timestamp, "")
+	globalCluster.UserSpecifiedSegPrefix = utils.ParseSegPrefix(*backupDir)
 	globalCluster.VerifyBackupDirectoriesExistOnAllHosts()
 
 	InitializeBackupConfig()

testutils/functions.go
@@ -76,7 +76,7 @@ func SetDefaultSegmentConfiguration() utils.Cluster {
 	configMaster := utils.SegConfig{ContentID: -1, Hostname: "localhost", DataDir: "gpseg-1"}
 	configSegOne := utils.SegConfig{ContentID: 0, Hostname: "localhost", DataDir: "gpseg0"}
 	configSegTwo := utils.SegConfig{ContentID: 1, Hostname: "localhost", DataDir: "gpseg1"}
-	cluster := utils.NewCluster([]utils.SegConfig{configMaster, configSegOne, configSegTwo}, "", "20170101010101")
+	cluster := utils.NewCluster([]utils.SegConfig{configMaster, configSegOne, configSegTwo}, "", "20170101010101", "gpseg")
 	return cluster
 }
 

utils/structs.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"os"
 	"os/user"
+	"path/filepath"
 	"time"
 
 	"github.com/jmoiron/sqlx"
@@ -74,6 +75,7 @@ type SystemFunctions struct {
 	CurrentUser   func() (*user.User, error)
 	Getenv        func(key string) string
 	Getpid        func() int
+	Glob          func(pattern string) (matches []string, err error)
 	Hostname      func() (string, error)
 	IsNotExist    func(err error) bool
 	MkdirAll      func(path string, perm os.FileMode) error
@@ -89,6 +91,7 @@ func InitializeSystemFunctions() *SystemFunctions {
 		CurrentUser:   user.Current,
 		Getenv:        os.Getenv,
 		Getpid:        os.Getpid,
+		Glob:          filepath.Glob,
 		Hostname:      os.Hostname,
 		IsNotExist:    os.IsNotExist,
 		MkdirAll:      os.MkdirAll,

commit f41362d6d85cce98b0bcd3b10e458d39f6131abd

Do not check for master backup directory if backupDir flag is not specified
This fixes an error introduced in 43ca011.

utils/cluster.go
@@ -349,12 +349,15 @@ func GetSegPrefix(connection *DBConn) string {
 }
 
 func ParseSegPrefix(backupDir string) string {
-	masterDir, err := System.Glob(fmt.Sprintf("%s/*-1", backupDir))
-	if err != nil || len(masterDir) == 0 {
-		logger.Fatal(err, "Master backup directory in %s missing or inaccessible", backupDir)
+	segPrefix := ""
+	if len(backupDir) > 0 {
+		masterDir, err := System.Glob(fmt.Sprintf("%s/*-1", backupDir))
+		if err != nil || len(masterDir) == 0 {
+			logger.Fatal(err, "Master backup directory in %s missing or inaccessible", backupDir)
+		}
+		_, segPrefix = path.Split(masterDir[0])
+		segPrefix = segPrefix[:len(segPrefix)-2] // Remove "-1" segment ID from string
 	}
-	_, segPrefix := path.Split(masterDir[0])
-	segPrefix = segPrefix[:len(segPrefix)-2] // Remove "-1" segment ID from string
 	return segPrefix
 }
 

utils/cluster_test.go
@@ -382,6 +382,9 @@ var _ = Describe("utils/cluster tests", func() {
 			utils.System.Glob = func(pattern string) (matches []string, err error) { return []string{"/tmp/foo/gpseg-1"}, nil }
 			Expect(utils.ParseSegPrefix("/tmp/foo")).To(Equal("gpseg"))
 		})
+		It("returns empty string if backup directory is empty", func() {
+			Expect(utils.ParseSegPrefix("")).To(Equal(""))
+		})
 		It("panics if master backup directory does not exist", func() {
 			utils.System.Glob = func(pattern string) (matches []string, err error) { return []string{}, nil }
 			defer testutils.ShouldPanicWithMessage("Master backup directory in /tmp/foo missing or inaccessible")