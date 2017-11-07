fd8c9877e7ae2cd6bea69666b7333b39a098fad8
Mike Roth <mroth@pivotal.io>
2017-11-07 15:40:15 -0500

    Initial import of conan file and build script

---
+++ conan/conanfile.py
@@ -0,0 +1,22 @@
+from conans import ConanFile, tools
+import os
+
+
+class GpbackupConan(ConanFile):
+    name = "gpbackup"
+    version = "0.2"
+    license = "Apache License v2.0"
+    url = "https://github.com/greenplum-db/gpbackup"
+    description = "Greenplum DB backup and restore utilities"
+    settings = "os", "compiler", "build_type", "arch"
+    exports_sources = "src/*", "src/github.com/greenplum-db/gpbackup/.git/*"
+
+    def build(self):
+        os.environ["PATH"] = os.environ["PATH"] + ":" + os.path.join(os.getcwd(), "bin")
+        with tools.environment_append({'GOPATH': os.getcwd()}):
+            with tools.chdir('src/github.com/greenplum-db/gpbackup'):
+                self.run('make build')
+
+    def package(self):
+        self.copy("gpbackup", dst="bin", src="bin")
+        self.copy("gprestore", dst="bin", src="bin")
---
+++ conan/export_conan.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ex
+
+GPBACKUP_SRC=${GOPATH}/src/github.com/greenplum-db/gpbackup
+pushd ${GPBACKUP_SRC}
+
+GIT_VER=$(git describe --tags | awk -F "-" '{$2+=0; print $1 "." $2}')
+DEV_VER=$(git diff | wc -l | awk '{if($1!=0) {print "+dev"}}')
+
+make depend
+
+sed -i '.bak' "s/^GIT_VERSION := .*/GIT_VERSION=${GIT_VER}/" Makefile
+sed -i '' "s/^DEV_VERSION := .*/DEV_VERSION=${DEV_VER}/" Makefile
+
+cp ./conan/conanfile.py $GOPATH
+popd
+pushd $GOPATH
+sed -i '' "s/version = .*/version = '${GIT_VER}${DEV_VER}'/" conanfile.py
+
+CONAN_VER=gpbackup/${GIT_VER}${DEV_VER}@gpdb/devel
+CONAN_REPO_NAME=zzz_gpdb_oss
+conan remove -f ${CONAN_VER}
+conan export ${CONAN_VER}
+conan install ${CONAN_VER} --build=missing
+conan remote add ${CONAN_REPO_NAME} https://api.bintray.com/conan/greenplum-db/gpdb-oss
+conan user -r ${CONAN_REPO_NAME} -p ${BINTRAY_TOKEN} ${BINTRAY_USER}
+conan upload -r ${CONAN_REPO_NAME} ${CONAN_VER}
+conan remote remove ${CONAN_REPO_NAME}
+mv ${GPBACKUP_SRC}/Makefile.bak ${GPBACKUP_SRC}/Makefile
