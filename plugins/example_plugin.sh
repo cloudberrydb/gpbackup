d442a5d84b6627e8ae397b9df4709d6efb979861
Soumyadeep Chakraborty <sochakraborty@pivotal.io>
2018-07-05 14:59:01 -0700

    Logging plugin method invocations
    Co-authored-by: Soumyadeep Chakraborty <sochakraborty@pivotal.io>
    Co-authored-by: Chris Hajas <chajas@pivotal.io>

--- plugins/example_plugin.sh
+++ plugins/example_plugin.sh
@@ -1,44 +1,50 @@
-#!/bin/bash
+  #!/bin/bash
 set -e
 
 setup_plugin_for_backup(){
+  echo "setup_plugin_for_backup $1 $2 $3" >> /tmp/plugin_out.txt
   mkdir -p /tmp/plugin_dest
 }
 
 setup_plugin_for_restore(){
-  :
+  echo "setup_plugin_for_restore $1 $2 $3" >> /tmp/plugin_out.txt
 }
 
 cleanup_plugin_for_backup(){
-  :
+  echo "cleanup_plugin_for_backup $1 $2 $3" >> /tmp/plugin_out.txt
 }
 
 cleanup_plugin_for_restore(){
-  :
+  echo "cleanup_plugin_for_restore $1 $2 $3" >> /tmp/plugin_out.txt
 }
 
 restore_file() {
+  echo "restore_file $1 $2" >> /tmp/plugin_out.txt
   filename=`basename "$2"`
 	cat /tmp/plugin_dest/$filename > $2
 }
 
 backup_file() {
+  echo "backup_file $1 $2" >> /tmp/plugin_out.txt
   filename=`basename "$2"`
 	cat $2 > /tmp/plugin_dest/$filename
 }
 
 backup_data() {
+  echo "backup_data $1 $2" >> /tmp/plugin_out.txt
   filename=`basename "$2"`
 	cat - > /tmp/plugin_dest/$filename
 }
 
 restore_data() {
+  echo "restore_data $1 $2" >> /tmp/plugin_out.txt
   filename=`basename "$2"`
 	cat /tmp/plugin_dest/$filename
 }
 
 plugin_api_version(){
   echo "0.2.0"
+  echo "0.2.0" >> /tmp/plugin_out.txt
 }
 
 "$@"
