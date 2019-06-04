df9dcc6b3f33500b7b47bad252055fa8f974c3b4
Larry Hamel <lhamel@pivotal.io>
2019-06-04 15:58:38 -0700

    Fix: allow `which golangci-lint` to fail without stopping script
    Co-authored-by: Trevor Yacovone <tyacovone@pivotal.io>
    Co-authored-by: Larry Hamel <lhamel@pivotal.io>

--- ci/scripts/install_linter.bash
+++ ci/scripts/install_linter.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-set -e
-
 LINTER=$(which golangci-lint)
 LINTER_VERSION=1.16.0
+
+set -eo pipefail
 
 if [[ "$LINTER" == "" ]] ; then
     wget -O - -q https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s v${LINTER_VERSION}
