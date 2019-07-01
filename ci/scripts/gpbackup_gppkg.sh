43557a9c42ef88e14458374822122f0f7cdac399
Junkeun Yi <juyi@pivotal.io>
2019-07-01 15:51:57 -0700

    Package all binaries into a single gppkg
    - Remove compilation of gpdb and building demo cluster. These steps are
      not required to use gppkg
    - Change RPM version to use '_' instead of '-'
    
    Co-authored-by: Junkeun Yi <juyi@pivotal.io>
    Co-authored-by: Kevin Yeap <kyeap@pivotal.io>
    Co-authored-by: Lav Jain <ljain@pivotal.io>

--- ci/scripts/gpbackup_gppkg.sh
+++ ci/scripts/gpbackup_gppkg.sh
@@ -7,15 +7,15 @@ if [ "$#" -ne 3 ]; then
     echo "./gpbackup_gppkg.sh [gpbackup version] [gpdb major version] [os]"
 fi
 
-GPBACKUP_VERSION=$1
-GPDB_MAJOR_VERSION=$2
-OS=$3
+export GPBACKUP_VERSION=$1
+export GPDB_MAJOR_VERSION=$2
+export OS=$3
 
 GPBACKUP_DIR=$(dirname $0)/../..
-RPMROOT=/tmp/gpbackup_rpm
+RPMROOT=/tmp/gpbackup_tools_rpm
 
 GPPKG_DEST_DIR=gpbackup_gppkg
-GPBACKUP_GPPKG=gpbackup-${GPBACKUP_VERSION}-gp${GPDB_MAJOR_VERSION}-${OS}-x86_64.gppkg
+GPBACKUP_GPPKG=gpbackup_tools-${GPBACKUP_VERSION}-gp${GPDB_MAJOR_VERSION}-${OS}-x86_64.gppkg
 
 # Create gppkg directory structure
 GPPKG_SOURCE_DIR=/tmp/gpbackup_gppkg
@@ -23,10 +23,8 @@ rm -rf ${GPPKG_SOURCE_DIR}
 mkdir -p ${GPPKG_SOURCE_DIR}/deps
 
 # Interpolate version values to create gppkg yaml file
-rm -f temp.yml
-( echo "cat <<EOF >${GPPKG_SOURCE_DIR}/gppkg_spec.yml";   cat ${GPBACKUP_DIR}/gppkg/gppkg_spec.yml.in;   echo "EOF"; ) >temp.yml
-. ./temp.yml
-rm -f temp.yml
+envsubst < ${GPBACKUP_DIR}/gppkg/gppkg_spec.yml.in > ${GPPKG_SOURCE_DIR}/gppkg_spec.yml
+cat ${GPPKG_SOURCE_DIR}/gppkg_spec.yml
 
 cp ${RPMROOT}/RPMS/x86_64/*.rpm ${GPPKG_SOURCE_DIR}
 gppkg --build ${GPPKG_SOURCE_DIR}
@@ -34,10 +32,9 @@ echo "Successfully built gppkg"
 
 if [ ${GPDB_MAJOR_VERSION} == '6' ] && [ ${OS} == "rhel6" ]; then
     echo "Testing installation of gpbackup using gppkg"
+
     GPBIN=${GPHOME}/bin
-    rm -f ${GPBIN}/gpbackup
-    rm -f ${GPBIN}/gprestore
-    rm -f ${GPBIN}/gpbackup_helper
+    rm -f ${GPBIN}/gpbackup ${GPBIN}/gprestore ${GPBIN}/gpbackup_helper
 
     gppkg -i ${GPBACKUP_GPPKG}
     if [ ! -f ${GPBIN}/gpbackup ] || [ ! -f ${GPBIN}/gprestore ] || [ ! -f ${GPBIN}/gpbackup_helper ]; then
--- ci/scripts/gpbackup_tools_rpm.sh
+++ ci/scripts/gpbackup_tools_rpm.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 set -ex
 
-# USAGE: ./gpbackup_rpm.sh [gpbackup version] [source targz file]
+# USAGE: ./gpbackup_rpm.sh [rpm version] [source targz file]
 # Example: ./gpbackup_rpm.sh 1.8.0 mybinaries.tar.gz
 if [ "$#" -ne 2 ]; then
-    echo "./gpbackup_rpm.sh [gpbackup version] [source targz file]"
+    echo "./gpbackup_tools_rpm.sh [rpm version] [source targz file]"
 fi
 
-GPBACKUP_VERSION=$1
+RPM_VERSION=$1
 SOURCE_TARGZ=$2
 
 GPBACKUP_DIR=$(dirname $0)/../..
 
 # Create rpm directory structure
-RPMROOT=/tmp/gpbackup_rpm
+RPMROOT=/tmp/gpbackup_tools_rpm
 rm -rf ${RPMROOT}
 mkdir -p ${RPMROOT}/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 
 # Move source targz to SOURCES
 cp ${SOURCE_TARGZ} ${RPMROOT}/SOURCES/.
-cp ${GPBACKUP_DIR}/gppkg/gpbackup.spec.in ${RPMROOT}/SPECS/gpbackup.spec
+cp ${GPBACKUP_DIR}/gppkg/gpbackup_tools.spec.in ${RPMROOT}/SPECS/gpbackup_tools.spec
 
-rpmbuild -bb ${RPMROOT}/SPECS/gpbackup.spec --define "%_topdir ${RPMROOT}" --define "debug_package %{nil}" --define "gpbackup_version $GPBACKUP_VERSION"
+rpmbuild -bb ${RPMROOT}/SPECS/gpbackup_tools.spec --define "%_topdir ${RPMROOT}" --define "debug_package %{nil}" --define "rpm_version $RPM_VERSION"
 
 echo "Successfully built RPM"
