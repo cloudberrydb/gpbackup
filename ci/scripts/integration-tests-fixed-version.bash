3a77513e60aeb4d18ea1b0b907ff1c1cb3216494
Kevin Yeap <kyeap@pivotal.io>
2019-12-13 15:40:28 -0800

    Use manually cached gpbackup 1.12.1 dependencies
    The gpbackup version 1.12.1 is tested against the latest version of GPDB5
    to ensure the version of gpbackup shipped with GPDB5X is not broken.
    Unfortunately, dep struggles to successfully download gpbackup
    dependencies sometimes so we implement a manual cache workaround. This
    commit unpacks the gpbackup 1.12.1 dependencies so it does not need to be
    downloaded by dep.
    
    Authored-by: Kevin Yeap <kyeap@pivotal.io>

--- ci/scripts/integration-tests-fixed-version.bash
+++ ci/scripts/integration-tests-fixed-version.bash
@@ -18,6 +18,8 @@ git checkout ${GPBACKUP_VERSION}
 # with gpdb5, we've decided to simply cherry-pick the commit prior to running tests.
 git cherry-pick c149e8b7b671e931ca892f22c8cdef906512d591
 
+tar -zxf ~/gpbackup_1.12.1_dependencies.tar.gz
+
 make depend build integration
 
 # NOTE: This is a temporary hotfix intended to skip these tests when running on CCP cluster
@@ -29,4 +31,7 @@ SCRIPT
 
 chmod +x /tmp/run_tests.bash
 scp /tmp/run_tests.bash mdw:/home/gpadmin/run_tests.bash
+
+rsync -a gpbackup_1.12.1_dependencies/gpbackup_1.12.1_dependencies.tar.gz mdw:/home/gpadmin
+
 ssh -t mdw "bash /home/gpadmin/run_tests.bash"
