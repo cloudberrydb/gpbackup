bce47f106ca8631f69d0052eb760e168d8f13d65
Lav Jain <ljain@pivotal.io>
2020-03-18 10:48:34 -0700

    Change dev pipeline template to remove sles/ubuntu task (#354)

--- ci/scripts/tar-gppkgs.bash
+++ ci/scripts/tar-gppkgs.bash
@@ -4,8 +4,12 @@ set -ex
 
 cp gpbackup-tools-versions/* gppkgs/
 mv rhel-gppkg/* gppkgs/
-mv sles-gppkg/* gppkgs/
-mv ubuntu-gppkg/* gppkgs/
+if [[ -d sles-gppkg ]]; then
+    mv sles-gppkg/* gppkgs/
+fi
+if [[ -d ubuntu-gppkg ]]; then
+    mv ubuntu-gppkg/* gppkgs/
+fi
 
 pushd gppkgs
     tar cvzf gpbackup-gppkgs.tar.gz *
