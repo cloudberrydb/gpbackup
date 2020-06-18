eb12da98beddea80b240948efc3760967433a6ff
Lav Jain <ljain@pivotal.io>
2020-06-18 00:04:57 -0700

    PivNet: Remove the -1 in naming of gpbackup files, releases and versions (#393)
    * PivNet: Remove the -1 in naming of gpbackup files, releases and versions
    * Remove any references to COUNT from gpbackup-tools-versions script

--- ci/scripts/gpbackup-tools-versions.bash
+++ ci/scripts/gpbackup-tools-versions.bash
@@ -8,27 +8,10 @@ pushd gpbackup-go-components
       GPBACKUP_VERSION=$(cat gpbackup_version)
       cp *_version ../gpbackup-tools-versions/
 popd
+echo ${GPBACKUP_VERSION} > gpbackup-tools-versions/pkg_version
 
 # get version for ddboost_plugin
 pushd gpbackup_ddboost_plugin
       DDBOOST_PLUGIN_VERSION=$(git describe --tags | perl -pe 's/(.*)-([0-9]*)-(g[0-9a-f]*)/\1+dev.\2.\3/')
 popd
 echo ${DDBOOST_PLUGIN_VERSION} > gpbackup-tools-versions/ddboost_plugin_version
-
-# get version for future .rpm and .deb files as well as .gppkg files
-pushd pivnet_release_cache
-  PRV_TILE_RELEASE_VERSION="v-${GPBACKUP_VERSION}*"
-  if [[ -f ${PRV_TILE_RELEASE_VERSION} ]]; then
-    # increment the counter if the expected release version has been used before
-    COUNT=$(echo ${PRV_TILE_RELEASE_VERSION} | sed -n "s/v-${GPBACKUP_VERSION}-\([0-9]*\).*/\1/p")
-    COUNT=$(($COUNT+1))
-  else
-    # reset the version count
-    COUNT=1
-  fi
-  # NEXT_GPBACKUP_TOOLS_VERSION is the tile release version with the `-` changed to a `+`
-  # because the `-` is reserved in RPM SPEC to denote `%{version}-%{release}` and the `_` is invalid symbol for .deb package name
-  NEXT_GPBACKUP_TOOLS_VERSION=${GPBACKUP_VERSION}+${COUNT}
-popd
-
-echo ${NEXT_GPBACKUP_TOOLS_VERSION} > gpbackup-tools-versions/pkg_version
--- ci/scripts/update-metadata.bash
+++ ci/scripts/update-metadata.bash
@@ -20,23 +20,19 @@ GPBACKUP_VERSION=$(cat workspace/files-to-upload/gpbackup_version)
 # if this release only includes updates to components outside of
 # gpbackup itself (e.g. plugins, manager)
 pushd pivnet_release_cache
-  PRV_TILE_VERSION=$(echo v-*)
-  CURR_TILE_VERSION="v-${GPBACKUP_VERSION}*"
+  PRV_TILE_VERSION=$(echo v-* | tr '+' '-' | cut -d- -f1-2)
+  CURR_TILE_VERSION=$(echo "v-${GPBACKUP_VERSION}" | tr '+' '-' | cut -d- -f1-2)
   if [[ -f ${CURR_TILE_VERSION} ]]; then
-    COUNT=$(echo ${CURR_TILE_VERSION} | sed -n "s/v-${GPBACKUP_VERSION}-\([0-9]*\).*/\1/p")
-    COUNT=$(($COUNT+1))
-    # If same gpbackup version, then not a major/minor release
-    RELEASE_TYPE="Maintenance"
+      echo "Release already exists on Pivnet"
+      exit 1
   else
-    # reset the version count
-    COUNT=1
     # detect the release type
     PRV_MAJOR=$(echo ${PRV_TILE_VERSION:2} | cut -d. -f1)
     PRV_MINOR=$(echo ${PRV_TILE_VERSION:2} | cut -d. -f2)
     PRV_PATCH=$(echo ${PRV_TILE_VERSION:2} | cut -d. -f3 | sed -r "s/([0-9]+).*/\1/")
-    CURR_MAJOR=$(echo ${GPBACKUP_VERSION} | cut -d. -f1)
-    CURR_MINOR=$(echo ${GPBACKUP_VERSION} | cut -d. -f2)
-    CURR_PATCH=$(echo ${GPBACKUP_VERSION} | cut -d. -f3)
+    CURR_MAJOR=$(echo ${CURR_TILE_VERSION:2} | cut -d. -f1)
+    CURR_MINOR=$(echo ${CURR_TILE_VERSION:2} | cut -d. -f2)
+    CURR_PATCH=$(echo ${CURR_TILE_VERSION:2} | cut -d. -f3 | sed -r "s/([0-9]+).*/\1/")
     if [[ "$PRV_MAJOR" != "$CURR_MAJOR" ]] ; then
       RELEASE_TYPE="Major"
     elif [[ "$PRV_MINOR" != "$CURR_MINOR" ]] ; then
@@ -49,17 +45,17 @@ pushd pivnet_release_cache
     fi
   fi
 
-  TILE_RELEASE_VERSION=${GPBACKUP_VERSION}-${COUNT}
+  TILE_RELEASE_VERSION=${CURR_TILE_VERSION:2}
   touch ../workspace/v-${TILE_RELEASE_VERSION}
 popd
 
-if test ! -f gpbackup-release-license/open_source_license_pivotal-gpdb-backup-${GPBACKUP_VERSION}-*.txt ; then
-  echo "License file for gpbackup version ${GPBACKUP_VERSION} does not exist in resource.\n Ensure the OSL is properly uploaded to the GCS bucket prior to pushing to pivnet." 1>&2
+if test ! -f gpbackup-release-license/open_source_license_pivotal-gpdb-backup-${CURR_MAJOR}.${CURR_MINOR}.*.txt ; then
+  echo "License file for gpbackup version ${CURR_MAJOR}.${CURR_MINOR}.* does not exist in resource.\n Ensure the OSL is properly uploaded to the GCS bucket prior to pushing to pivnet." 1>&2
   exit 1
 fi
-cp gpbackup-release-license/open_source_license_pivotal-gpdb-backup-${GPBACKUP_VERSION}-*.txt workspace/files-to-upload/
+cp gpbackup-release-license/open_source_license_pivotal-gpdb-backup-*.txt workspace/files-to-upload/
 
-# NOTE: We must use the Pivnet Release Version (including the counter) because we cannot upload files with the same name in different tile releases
+# NOTE: We must use the Pivnet Release Version because we cannot upload files with the same name in different tile releases
 DDBOOST_PLUGIN_VERSION=$(cat workspace/files-to-upload/ddboost_plugin_version)
 sed -i "s/<DDBOOST_PLUGIN_VERSION>/${DDBOOST_PLUGIN_VERSION}/g" workspace/metadata.yml
 S3_PLUGIN_VERSION=$(cat workspace/files-to-upload/s3_plugin_version)
@@ -67,8 +63,8 @@ sed -i "s/<S3_PLUGIN_VERSION>/${S3_PLUGIN_VERSION}/g" workspace/metadata.yml
 BMAN_VERSION=$(cat workspace/files-to-upload/gpbackup_manager_version)
 sed -i "s/<BMAN_VERSION>/${BMAN_VERSION}/g" workspace/metadata.yml
 sed -i "s/<TILE_RELEASE_VERSION>/${TILE_RELEASE_VERSION}/g" workspace/metadata.yml
-sed -i "s/<GPBAR_VERSION>/${GPBACKUP_VERSION}/g" workspace/metadata.yml
-OSL_FILENAME=$(basename -- gpbackup-release-license/open_source_license_pivotal-gpdb-backup-${GPBACKUP_VERSION}-*.txt)
+sed -i "s/<GPBAR_VERSION>/${TILE_RELEASE_VERSION}/g" workspace/metadata.yml
+OSL_FILENAME=$(basename -- gpbackup-release-license/open_source_license_pivotal-gpdb-backup-*.txt)
 sed -i "s/<OSL_FILENAME>/${OSL_FILENAME}/g" workspace/metadata.yml
 sed -i "s/<RELEASE_TYPE>/${RELEASE_TYPE} Release/g" workspace/metadata.yml
 
@@ -78,15 +74,17 @@ END_OF_SUPPORT_DATE=$(date -d "$future_date_18_month - 1 day" +%Y-%m-%d)
 sed -i "s/<END_OF_SUPPORT_DATE>/${END_OF_SUPPORT_DATE}/g" workspace/metadata.yml
 
 # The URL adjusts based on major/minor version
-REL_NOTES_VERSION=$(echo ${GPBACKUP_VERSION//./-} | cut -d- -f1,2)
+REL_NOTES_VERSION=$(echo ${TILE_RELEASE_VERSION//./-} | cut -d- -f1,2)
 sed -i "s/<REL_NOTES_VERSION>/${REL_NOTES_VERSION}/g" workspace/metadata.yml
 
 cat workspace/metadata.yml
 pushd workspace/files-to-upload
   # rename files to match the name inside metadata.yml
   for filename in *.gppkg; do
-    newFilename=$(sed -e "s/gpbackup_tools-${GPBACKUP_VERSION}+[0-9]*/pivotal_greenplum_backup_restore-${TILE_RELEASE_VERSION}/" -e "s/RHEL/rhel/" -e "s/SLES/sles/" <<< "$filename")
-    mv "$filename" "$newFilename"
+    newFilename=$(sed -e "s/gpbackup_tools-[+0-9a-z.]*/pivotal_greenplum_backup_restore-${TILE_RELEASE_VERSION}/" -e "s/RHEL/rhel/" -e "s/SLES/sles/" <<< "$filename")
+    if ! [ "$filename" == "$newFilename" ]; then
+      mv "$filename" "$newFilename"
+    fi
   done
   # rename binary tarball
   mv "bin_gpbackup.tar.gz" "pivotal_greenplum_backup_restore-${TILE_RELEASE_VERSION}.tar.gz"
