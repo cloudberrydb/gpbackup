1371ac9bb8c4f148375071c207ca7ed3655539ce
Kevin Yeap <kyeap@vmware.com>
2021-11-08 11:30:37 -0800

    Add test for TerminateHangingCopySessions
    Adds a test for the previous commit.
    
    gpbackup commit reference:
    https://github.com/greenplum-db/gpbackup/commit/d52eeb71f3e120f2d318eb6c29e009f18981d69a
    
    Co-authored-by: Kevin Yeap <kyeap@vmware.com>
    Co-authored-by: Brent Doil <bdoil@vmware.com>

---
+++ integration/utils_test.go
@@ -0,0 +1,56 @@
+package integration
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/greenplum-db/gp-common-go-libs/dbconn"
+	"github.com/greenplum-db/gp-common-go-libs/testhelper"
+	fp "github.com/greenplum-db/gpbackup/filepath"
+	"github.com/greenplum-db/gpbackup/testutils"
+	"github.com/greenplum-db/gpbackup/utils"
+
+	"golang.org/x/sys/unix"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("utils integration", func() {
+	It("TerminateHangingCopySessions kills hanging COPY sessions", func() {
+		tempDir, err := ioutil.TempDir("", "temp")
+		Expect(err).To(Not(HaveOccurred()))
+		defer os.Remove(tempDir)
+		testPipe := filepath.Join(tempDir, "test_pipe")
+		conn := testutils.SetupTestDbConn("testdb")
+		defer conn.Close()
+
+		fpInfo := fp.FilePathInfo{
+			PID:       1,
+			Timestamp: "11223344556677",
+		}
+
+		testhelper.AssertQueryRuns(conn, "SET application_name TO 'hangingApplication'")
+		testhelper.AssertQueryRuns(conn, "CREATE TABLE public.foo(i int)")
+		defer testhelper.AssertQueryRuns(conn, "DROP TABLE public.foo")
+		err = unix.Mkfifo(testPipe, 0777)
+		Expect(err).To(Not(HaveOccurred()))
+		defer os.Remove(testPipe)
+		go func() {
+			copyFileName := fpInfo.GetSegmentPipePathForCopyCommand()
+			// COPY will blcok because there is no reader for the testPipe
+			_, _ = conn.Exec(fmt.Sprintf("COPY public.foo TO PROGRAM 'echo %s > /dev/null; cat - > %s' WITH CSV DELIMITER ','", copyFileName, testPipe))
+		}()
+
+		query := `SELECT count(*) FROM pg_stat_activity WHERE application_name = 'hangingApplication'`
+		Eventually(func() string { return dbconn.MustSelectString(connectionPool, query) }, 5*time.Second, 100*time.Millisecond).Should(Equal("1"))
+
+		utils.TerminateHangingCopySessions(connectionPool, fpInfo, "hangingApplication")
+
+		Eventually(func() string { return dbconn.MustSelectString(connectionPool, query) }, 5*time.Second, 100*time.Millisecond).Should(Equal("0"))
+
+	})
+})
