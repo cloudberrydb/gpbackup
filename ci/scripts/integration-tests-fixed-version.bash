997b6196718c344968ed2992218830cb67357985
Brent Doil <bdoil@vmware.com>
2022-01-25 15:38:30 -0500

    Update setup process for gpbackup 1.12.1 integration tests
    Authored-by: Brent Doil <bdoil@vmware.com>

--- ci/scripts/integration-tests-fixed-version.bash
+++ ci/scripts/integration-tests-fixed-version.bash
@@ -2,12 +2,36 @@
 
 set -ex
 
-ccp_src/scripts/setup_ssh_to_cluster.sh
+if [[ ! -f bin_gpdb/bin_gpdb.tar.gz ]] ; then
+  mv bin_gpdb/*.tar.gz bin_gpdb/bin_gpdb.tar.gz
+fi
+source gpdb_src/concourse/scripts/common.bash
+time install_gpdb
+time ./gpdb_src/concourse/scripts/setup_gpadmin_user.bash
+time make_cluster
+
+CWDIR=$(pwd)
+GOPATH=/home/gpadmin/go
+
+mkdir -p ${GOPATH}/bin ${GOPATH}/src/github.com/greenplum-db
+cp -R $(pwd)/gpbackup ${GOPATH}/src/github.com/greenplum-db
+tar -zxf gpbackup_1.12.1_dependencies/*.tar.gz -C ${GOPATH}/src/github.com/greenplum-db/gpbackup
+chown -R gpadmin:gpadmin ${GOPATH}
+
+cat<<SCRIPT > /home/gpadmin/.bashrc
+export GOPATH=${GOPATH}
+export PATH=/usr/local/go/bin:$PATH:${GOPATH}/bin
+if [[ -f /opt/gcc_env.sh ]]; then
+    source /opt/gcc_env.sh
+fi
+source /usr/local/greenplum-db-devel/greenplum_path.sh
+source ${CWDIR}/gpdb_src/gpAux/gpdemo/gpdemo-env.sh
+SCRIPT
 
 cat <<SCRIPT > /tmp/run_tests.bash
 set -ex
 
-source env.sh
+#!/bin/bash
 
 cd \${GOPATH}/src/github.com/greenplum-db/gpbackup
 
@@ -16,24 +40,17 @@ git checkout ${GPBACKUP_VERSION}
 # NOTE: There was a change to constraint handling in GPDB5 that caused an update
 # to our test suite. Rather than revv the version of gpbackup that we are packaging
 # with gpdb5, we've decided to simply cherry-pick the commit prior to running tests.
-git cherry-pick c149e8b7b671e931ca892f22c8cdef906512d591
+git checkout c149e8b7b671e931ca892f22c8cdef906512d591
 
-tar -zxf ~/gpbackup_1.12.1_dependencies.tar.gz
+tar -zxf ${CWDIR}/gpbackup_1.12.1_dependencies/*.tar.gz
+go install github.com/onsi/ginkgo/ginkgo@v1
 
+# Disable go modules before building
 export GO111MODULE=off
-go get github.com/onsi/ginkgo/ginkgo
 make build integration
-
-# NOTE: This is a temporary hotfix intended to skip these tests when running on CCP cluster
-#       because the backup artifact that these tests are using only works on local clusters.
-sed -i 's|\tIt\(.*\)\(--on-error-continue\)|\tPIt\1\2|' end_to_end/end_to_end_suite_test.go
 
 make end_to_end
 SCRIPT
 
 chmod +x /tmp/run_tests.bash
-scp /tmp/run_tests.bash mdw:/home/gpadmin/run_tests.bash
-
-rsync -a gpbackup_1.12.1_dependencies/gpbackup_1.12.1_dependencies.tar.gz mdw:/home/gpadmin
-
-ssh -t mdw "bash /home/gpadmin/run_tests.bash"
+su - gpadmin "/tmp/run_tests.bash"
